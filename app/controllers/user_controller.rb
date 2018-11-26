5a8c5132dab0e8b33ecd566a94fb22e70d128845
critter42 <critter42@gmail.com>
2018-11-25 23:49:36 -0600

    Added Show User Pins

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,9 +1,3 @@
 class UserController < ApplicationController
-    def home
-        begin
-            @user = User.find(params[:id])
-        rescue
-            redirect_to home_url, alert: 'Error: Unable to locate pins'
-        end
-        @pothole = @user.pins.find(@user.users_ids)
+    
 end
